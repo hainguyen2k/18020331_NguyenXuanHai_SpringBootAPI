d93d6171c7481bd28984de180ce8e28ffed0c428
hainguyen2k <ngxuanhai1000@gmail.com>
2021-09-20 22:57:50 +0700

    Add validation

--- src/main/java/com/example/n18020331_nguyenxuanhai_springbootapi/controller/StudentController.java
+++ src/main/java/com/example/n18020331_nguyenxuanhai_springbootapi/controller/StudentController.java
@@ -4,37 +4,48 @@ import com.example.n18020331_nguyenxuanhai_springbootapi.entity.Student;
 import com.example.n18020331_nguyenxuanhai_springbootapi.repository.StudentRepository;
 import com.example.n18020331_nguyenxuanhai_springbootapi.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
+
 
 @RestController
 public class StudentController {
     @Autowired
     private StudentService studentService;
 
-    @PostMapping("/students")
-    public Student Save(@RequestBody Student student){
-        return studentService.Save(student);
+    //    @PostMapping("/students")
+//    public Student Save(@RequestBody Student student){
+//        return studentService.Save(student);
+//    }
+    @PostMapping("/employees")
+    ResponseEntity<Student> saveEmployee(@Valid  @RequestBody Student student) {
+        return new ResponseEntity<Student>(studentService.Save(student), HttpStatus.CREATED);
     }
+
     @GetMapping("/students")
-    public List<Student> GetAll(){
+    public List<Student> GetAll() {
         return studentService.GetAll();
     }
+
     @GetMapping("/students/{id}")
-    public Student GetById(@PathVariable("id") Long id){
+    public Student GetById(@PathVariable("id") Long id) {
         return studentService.GetById(id);
     }
+
     @DeleteMapping("/students/{id}")
-    String DeleteById(@PathVariable("id") Long id){
+    String DeleteById(@PathVariable("id") Long id) {
         studentService.DeleteById(id);
         return "OK";
     }
+
     @PutMapping("/students/{id}")
-    Student UpdateById(@PathVariable("id") Long id, @RequestBody Student student){
+    Student UpdateById(@PathVariable("id") Long id, @RequestBody Student student) {
         return studentService.UpdateById(id, student);
     }
-
 
 
 }
--- src/main/java/com/example/n18020331_nguyenxuanhai_springbootapi/entity/Student.java
+++ src/main/java/com/example/n18020331_nguyenxuanhai_springbootapi/entity/Student.java
@@ -1,17 +1,19 @@
 package com.example.n18020331_nguyenxuanhai_springbootapi.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 
 @Entity
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
 @Builder
+@Getter
+@Setter
 @Table(name = "tbl_student")
 public class Student {
 
@@ -19,10 +21,13 @@ public class Student {
     @GeneratedValue
     private Long id;
 
+    @NotBlank(message = "Please input your first name!")
+    @Length(max = 10, min = 1, message = "Less than ten and greater than one")
     @Column(name = "first_name")
     private String firstName;
     @Column(name = "last_name")
     private String  lastName;
+    @Email(message = "Email is not valid")
     @Column(name = "email")
     private String email;
 }
